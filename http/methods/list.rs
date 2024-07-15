dbf36570432f7c5c38483cb80c86bd0b1090cef1
urizennnn <igamerryt@gmail.com>
2024-07-15 02:56:31 +0100

    chore:added workflows

--- http/methods/list.rs
+++ http/methods/list.rs
@@ -1,9 +1,23 @@
 use std::error::Error;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::fs;
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpStream;
 
-pub async fn list_storage(stream: &mut TcpStream) -> Result<String, Box<dyn Error>> {
+pub async fn list_storage(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
     stream.write_all(b"Listing storage items...\n").await?;
+
+    let mut content = String::new();
+    let entries = fs::read_dir("storage")?;
+    for entry in entries {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        let file_name_str = file_name.to_string_lossy();
+        content.push_str(&file_name_str);
+        content.push('\n');
+    }
+
+    stream.write_all(content.as_bytes()).await?;
     stream.flush().await?;
-    Ok("Storage items listed".to_string())
+
+    Ok(())
 }
--- tcp/tcp.rs
+++ tcp/tcp.rs
@@ -4,7 +4,6 @@ use crate::http::put::put;
 use crate::threadpool::thread::Threadpool;
 use std::error::Error;
 use std::process::exit;
-use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 
@@ -15,16 +14,15 @@ impl TCP {
         let listener = TcpListener::bind(addr).await?;
         println!("Server listening on {}", addr);
 
-        let pool = Arc::new(Threadpool::build(6).unwrap_or_else(|_| {
+        let pool = Threadpool::build(6).unwrap_or_else(|_| {
             eprintln!("Failed to create thread pool");
             exit(1);
-        }));
+        });
 
         loop {
             match listener.accept().await {
                 Ok((stream, _)) => {
-                    let pool = Arc::clone(&pool);
-                    pool.execute(move || {
+                    pool.execute(|| {
                         tokio::spawn(async move {
                             if let Err(e) = TCP::handle_client(stream).await {
                                 eprintln!("Error handling client: {}", e);
@@ -40,67 +38,40 @@ impl TCP {
     async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
         let mut buffer = vec![0; 5_242_880];
         loop {
-            let n = match stream.read(&mut buffer).await {
-                Ok(n) => n,
-                Err(e) => {
-                    eprintln!("Failed to read from stream: {}", e);
-                    continue;
-                }
-            };
-
+            let n = stream.read(&mut buffer).await?;
             if n == 0 {
                 println!("Connection closed by client");
                 break;
             }
-
-            let request = Self::parse_request(&buffer, n)?;
+            let request = String::from_utf8_lossy(&buffer[..n])
+                .trim_matches(char::from(0))
+                .trim()
+                .to_string();
+            println!("Received request: {}", request);
 
             match AllowedRequest::from_str(&request) {
-                Some(AllowedRequest::Put) => Self::process_put(&mut stream, &mut buffer).await?,
-                Some(AllowedRequest::List) => Self::process_list(&mut stream).await?,
-                Some(AllowedRequest::Delete) => Self::process_delete(&request),
-                Some(AllowedRequest::Get) => Self::process_get(&request),
+                Some(AllowedRequest::Put) => {
+                    put(&mut stream, &mut buffer).await?;
+                }
+                Some(AllowedRequest::List) => {
+                    list::list_storage(&mut stream).await?;
+                }
+                Some(AllowedRequest::Delete) => {
+                    println!("Processing DELETE request");
+                }
+                Some(AllowedRequest::Get) => {
+                    println!("Processing GET request");
+                }
                 None => {
-                    eprintln!("Unsupported request: {}", request);
+                    println!("Unsupported request: {}", request);
                     stream.write_all(b"Unsupported request\n").await?;
+                    stream.flush().await?;
+                    // return Err("Unsupported request: {}".into());
                 }
             }
 
-            if let Err(e) = stream.flush().await {
-                eprintln!("Failed to flush stream: {}", e);
-            }
+            stream.flush().await?;
         }
         Ok(())
-    }
-
-    fn parse_request(buffer: &[u8], n: usize) -> Result<String, Box<dyn Error>> {
-        let request = String::from_utf8_lossy(&buffer[..n])
-            .trim_matches(char::from(0))
-            .trim()
-            .to_string();
-        Ok(request)
-    }
-
-    async fn process_put(
-        stream: &mut TcpStream,
-        buffer: &mut Vec<u8>,
-    ) -> Result<(), Box<dyn Error>> {
-        put(stream, buffer).await?;
-        Ok(())
-    }
-
-    async fn process_list(stream: &mut TcpStream) -> Result<(), Box<dyn Error>> {
-        list::list_storage(stream).await?;
-        Ok(())
-    }
-
-    fn process_delete(request: &str) {
-        println!("Processing DELETE request: {}", request);
-        // Implement actual delete logic here
-    }
-
-    fn process_get(request: &str) {
-        println!("Processing GET request: {}", request);
-        // Implement actual get logic here
     }
 }
