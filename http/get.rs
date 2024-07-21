72c36787f3d95b5f5f43af3674e681f4ce5b03db
urizennnn <igamerryt@gmail.com>
2024-07-21 01:09:40 +0100

    chore:fixed send to client

--- http/get.rs
+++ http/get.rs
@@ -9,26 +9,44 @@ use tokio::{
 
 pub async fn get_file(stream: &mut TcpStream, buffer: &mut [u8]) -> Result<(), Box<dyn Error>> {
     info!("Getting file..");
-    let buf_string = String::from_utf8_lossy(&buffer[..])
+    let buf_string = String::from_utf8_lossy(buffer)
         .trim_matches(char::from(0))
         .trim()
         .to_string();
     let parts: Vec<&str> = buf_string.split_whitespace().collect();
 
-    if parts.len() != 2 {
+    if parts.len() != 3 {
         stream.write_all(b"Invalid Command").await?;
         return Err("Invalid command".into());
     }
     let raw_path = parts[1];
     let format_path = format!("storage/{raw_path}");
     let path = Path::new(&format_path);
-    let file = File::open(&path).await?;
+    let mut file = File::open(&path).await?;
     let file_size = file.metadata().await?.len();
     let response = format!("Details:{format_path:?} {:?}", file_size);
     println!("{response}");
-    stream.write_all(response.as_bytes()).await?;
-    stream.write_all(b"\n").await?;
+    stream
+        .write_all(format!("SEND {} {}\n", format_path, file_size).as_bytes())
+        .await?;
     stream.flush().await?;
+
+    let mut total_sent = 0;
+    loop {
+        let bytes_read = file.read(buffer).await?;
+        if bytes_read == 0 {
+            break;
+        }
+        stream.write_all(&buffer[..bytes_read]).await?;
+        stream.flush().await?;
+
+        total_sent += bytes_read;
+        info!("Progress: {}/{} bytes", total_sent, file_size);
+    }
+
+    info!("Upload complete: {} bytes sent", total_sent);
+
     info!("File sent");
+
     Ok(())
 }
